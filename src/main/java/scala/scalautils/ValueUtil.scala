4b558aaf47d6bd496a064f8b58539d49bd9c7c99
fjl121029xx <sishengqikuo_xx@163.com>
2018-06-20 18:38:36 +0800

    2018-06-20  基本完成，准备SCALA版

--- src/main/java/scala/scalautils/ValueUtil.scala
+++ src/main/java/scala/scalautils/ValueUtil.scala
@@ -7,7 +7,7 @@ object ValueUtil {
 
   val sdf = new SimpleDateFormat("yyyy-MM-dd")
 
-  def parseVal2Long(str: String, field: String): Long = {
+  def parseVal2Long(str: String, field: String) = {
 
     val fields = str.split("\\|")
 
@@ -16,13 +16,13 @@ object ValueUtil {
       var f = fieldsIte.next()
 
       if (f.startsWith(field)) {
-        return Long.unbox(f.split("=")(1))
+        Long.unbox(f.split("=")(1))
       }
     }
     0
   }
 
-  def parseVal2Int(str: String, field: String): Int = {
+  def parseVal2Int(str: String, field: String) = {
 
     val fields = str.split("\\|")
 
@@ -31,13 +31,13 @@ object ValueUtil {
       var f = fieldsIte.next()
 
       if (f.startsWith(field)) {
-        return Int.unbox(f.split("=")(1))
+        return Int.unbox(f.split("=")(1)): Int
       }
     }
-    0
+    0: Int
   }
 
-  def parseVal2Str(str: String, field: String): String = {
+  def parseVal2Str(str: String, field: String) = {
 
     val fields = str.split("\\|")
 
@@ -49,10 +49,10 @@ object ValueUtil {
         return f.split("=")(1)
       }
     }
-    ""
+    "": String
   }
 
-  def parseVal2Date(str: String, field: String): String = {
+  def parseVal2Date(str: String, field: String) = {
 
     val fields = str.split("\\|")
 
